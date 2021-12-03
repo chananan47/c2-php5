707a501642e814458f1c513805fd0a05497df734
chananan47 <namayura47@gmail.com>
2021-12-03 11:34:24 +0700

    initial commit

---
+++ php501.php
@@ -0,0 +1,27 @@
+<?php
+
+$season = array("summer" , "winter" , "spring" , "autumn",);
+
+echo $season[0];
+echo "<br />";
+echo $season[1];
+echo "<br />";
+echo $season[2];
+echo "<br />";
+echo $season[3];
+echo "<br />";
+echo "****************";
+echo "<br />";
+
+?>
+
+<?php
+//$country = array("Thailand","Malaysia","Singapore");
+$country[0] = "Thailand";
+$country[1] = "Malaysia";
+$country[2] = "Singapore";
+echo $country[0];
+echo "<br />";
+echo $country[1];
+echo "<br />";
+echo $country[2];
---
+++ php503.php
@@ -0,0 +1,10 @@
+<?php
+
+$salary = array("Mark"=>"35000","Jhon"=>"45000","Jeff"=>"55000");
+
+// echo "$salary[Mark]<br />";
+// echo "$salary[Jhon]<br />";
+// echo $salary["Jeff"];
+for($i = 0; $i < count($salary); $i++){
+    echo $salary[$i];
+}
